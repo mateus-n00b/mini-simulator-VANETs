84641df76a10e25de1d75031bec071c9412ac921
Mateus Sousa <mateus.aluufc@gmail.com>
2018-04-23 10:31:14 -0300

    New updates

--- trace_2_ns2.py
+++ trace_2_ns2.py
@@ -6,15 +6,15 @@
 # License: GPLv3+
 # TODO: Test the limits of Class car (i.e., how many objects it supports?)
 import csv,sys
-import car # Class car that creates a object for every node in the tracefile
+import car # Class car creates a representative object for every node in the tracefile
 from optparse import OptionParser
 
-nodecontainer = dict()
+nodecontainer = dict() # Store the nodes created
 
 def parser(tracefile,max_vehicles): # Read the tracefile and fills nodecontainer with vehicles objects
     global nodecontainer # Structure: {'vehicle_id':object}
-
     node_id = int() # Var used to attribute a new id for each vehicle
+
     with open(tracefile, 'rb') as csvfile:
         fp =  csv.DictReader(csvfile)
         for row in fp:
@@ -67,7 +67,7 @@ def createTracefile(output): # Read the mobility infos in nodecontainer and crea
                 to_str = ""
 
                 if intervals == '0.00' and speed == '0.00':
-                    to_str = "$node_({0}) set X_ {1} ;\n\
+                    to_str = "$node_({0}) set X_ {1} ;#ref\n\
 $node_({0}) set Y_ {2} ;#ref\n\
 $ns_ at {3} \"$node_({0}) setdest {1} {2} {4}\"\n\
                               ".format(node_id,posX,posY,intervals,speed)
@@ -75,8 +75,7 @@ $ns_ at {3} \"$node_({0}) setdest {1} {2} {4}\"\n\
                     to_str = "$ns_ at {1} \"$node_({0}) setdest {2} {3} {4}\"\n".format(node_id,intervals,
                     posX,posY,speed)
 
-                tracefile.write(to_str)
-
+                tracefile.write(to_str) # Writes in the tracefile
     tracefile.close()
 
 def main():
@@ -87,15 +86,14 @@ def main():
                     help="output file (ns-2 tracefile)")
     parser_.add_option("-n","--nodes",dest="n_nodes",type="int",default=0,
                     help="number number of nodes")
-
     opt,args = parser_.parse_args()
 
     if not opt.tracefile:
         print "[-] Invalid entry! Try {0} -h".format(sys.argv[0])
         exit(-1)
 
-    parser(opt.tracefile,opt.n_nodes)
     print "*** Saving output in \"{0}\"".format(opt.output)
+    parser(opt.tracefile,opt.n_nodes)
     createTracefile(output=opt.output)
 
 if __name__ == '__main__':
